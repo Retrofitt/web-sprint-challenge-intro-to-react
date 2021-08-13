6b4c75c6f8fbf7fb210a6b0ba0de85a743940ca6
Retrofitt <rafaelmendozajr94@gmail.com>
2021-08-13 14:14:53 -0700

    sprint-challenge-done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+import axios from 'axios'
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+import Character from './components/Character'
+
+const App = () => {
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://swapi.dev/api/people')
+    .then(res=>{
+      setCharacters(res.data)
+    }) 
+    .catch(err=>{
+    console.error(err)
+    })
+  }, [])
+
+  
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(char =>{
+        return <Character 
+        char={char}/>})
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+
+import styled from 'styled-components'
+
+
+ export default function Character(props){
+    const {char} = props;
+
+
+    const CharacterDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    `
+
+    const NameBorders = styled.p`
+    display: flex;
+    color: white;
+    background-color:lightcoral;
+    justify-content: center;
+    align-items: center;
+    height: 3vh;
+    width: 20%;
+    border-radius: 5px;
+    `
+
+    return (
+        <CharacterDiv className='Names'>
+            <NameBorders>{char.name}</NameBorders>
+        </CharacterDiv>
+        )
+}
